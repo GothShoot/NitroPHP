a8ad0c098ed528af18b381d63b7b352cd801a172
GothShoot <34012335+GothShoot@users.noreply.github.com>
2019-01-08 10:48:37 +0100

    [Improve] Core massive update. :construction_worker:

--- Core/ConfigLoader.php
+++ Core/ConfigLoader.php
@@ -1,19 +1,52 @@
 <?php
-class ConfigLoader
+
+namespace Core;
+
+use Core\Singleton;
+
+class ConfigLoader extends Singleton
 {
-    public function loadJsonConfigGroup($path, $file = null)
+    private $appconf;
+
+    public function getAppConf()
     {
-        $path = ROOT_DIR .'/Config'. $path . '/';
+        if(!$this->appconf){
+            $this->appconf = $this->loadJsonConfig(ROOT_DIR.'/Config/', 'app.json');
+        }
+        return $this->appconf;
+    }
+
+    public function loadJsonConfig(string $path, string $file = null):array
+    {
         if($file) {
             return json_decode(file_get_contents($path . $file), true);
         }
         $raw_files = scandir($path);
+        $files = [];
         foreach($raw_files as $file){
-            if (is_file($path . $file)) $files[] = $path . $file;
+            if (pathinfo($path . $file, PATHINFO_EXTENSION) == 'json') $files[] = $path . $file;
         }
+        $configs = [];
         foreach($files as $file) {
-            $configs = array_merge($configs, json_decode(file_get_contents($file[$o]), true));
+            $configs = array_merge($configs, json_decode(file_get_contents($file), true));
         }
         return $configs;
+    }
+
+    public function listModule(bool $force = null):array
+    {
+        if( !file_exists( (ROOT_DIR.'/Var/Cache/App') ) ){mkdir(ROOT_DIR.'/Var/Cache/App', 0775, true);}
+        if( !file_exists(ROOT_DIR.'/Var/Cache/App/module.json') || $force || $this->appconf['dev'] ){
+            $raw_files = scandir( ROOT_DIR.'/Module/' );
+            $files = [];
+            foreach($raw_files as $file){
+                if ($file != '.' && $file != '..') {
+                    $files[$file] = $this->loadJsonConfig(ROOT_DIR.'/Module/'.$file.'/Config/');
+                    $files[$file]['file'] = $file;
+                }
+            }
+            file_put_contents(ROOT_DIR.'/Var/Cache/App/module.json', json_encode($files));
+        }
+        return json_decode(file_get_contents(ROOT_DIR.'/Var/Cache/App/module.json'), true);
     }
 }
--- Core/Controller.php
+++ Core/Controller.php
@@ -2,19 +2,75 @@
 
 namespace Core;
 
+use Core\ConfigLoader;
+use Twig_Loader_Filesystem;
+use Twig_Environment;
+
+
 class Controller
 {
+    private static $_instance;
+
+    private $twig;
+
+    protected $appconf;
+
+    protected $configLoader;
+
     public function __construct()
     {
-        global $kernel;
-        $this->twig = $kernel->loadTwig();
+        $this->configLoader = ConfigLoader::getInstance();
+        $this->appconf = $this->configLoader->getAppConf();
     }
-    public function render($view, $param=[])
+
+    private function __clone()
     {
-        return $this->twig->render($view, $param);
+        trigger_error("Le clonage n'est pas autorisé.", E_USER_ERROR);
     }
-    public function renderblock($view, $param=[], $block)
+
+    public static function getInstance() {
+        if (!(static::$_instance instanceof static)) {static::$_instance = new self();}
+        return static::$_instance;
+    }
+
+    private function loadTwig()
     {
+        if(isset($this->twig) == false){
+            if( !file_exists(ROOT_DIR.'/Var/Cache/App/twig.cache') || $this->appconf['dev'] ){
+                $loader = new Twig_Loader_Filesystem();
+                foreach($this->configLoader->listModule() as $module){
+                    if($module['enabled']){
+                        $loader->addPath(ROOT_DIR.'/Module/'.$module['name'].'/View', $module['name']);
+                    }
+                }
+                $twig = new Twig_Environment($loader, array(
+                    'cache' => ($this->appconf['cachemode'] ? ROOT_DIR.'/Var/Cache/View' : false),
+                    'auto_reload' => ($this->appconf['dev'])
+                ));
+                // $twig->addExtension(new Twig_Extension_Core());
+                // $twig->addExtension(new Twig_Extension_Escaper('html'));
+                // if($this->appconf['dev']){
+                //     $profile = new Twig_Profiler_Profile();
+                //     $twig->addExtension(new Twig_Extension_Profiler($profile));
+                //     $dumper = new Twig_Profiler_Dumper_Text();
+                // } else {
+                //     $twig->addExtension(new Twig_Extension_Optimizer());
+                // }
+                file_put_contents(ROOT_DIR.'/Var/Cache/App/twig.cache', serialize($twig));
+            }
+            $this->twig =  unserialize(file_get_contents(ROOT_DIR.'/Var/Cache/App/twig.cache'));
+        }
+    }
+
+    protected function render(string $view, array $param=[])
+    {
+        $this->loadTwig();
+        echo $this->twig->render($view, $param);
+    }
+
+    protected function renderblock(string $view,array $param=[],string $block)
+    {
+        $this->loadTwig();
         return $this->twig->render($view, $param);
     }
 }
