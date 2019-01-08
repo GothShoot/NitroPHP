b0b95c866f99efdb51b3bacfeb722a5d040aa0bb
GothShoot <34012335+GothShoot@users.noreply.github.com>
2019-01-08 10:49:12 +0100

    [Feat] Routing. :page_with_curl:

--- Core/Router.php
+++ Core/Router.php
@@ -1,18 +1,61 @@
 <?php
 
-use ConfigLoader;
+namespace Core;
+
+use Core\ConfigLoader;
 
 class Router
 {
-    private function parse($url, $request)
+    private $start;
+
+    public function __construct()
     {
-        $routes = ConfigLoader::loadConfig('Routes');
-        foreach($routes as $route){
-            if($route == $url) {
-                return $route->controller;
+        $this->start = microtime(true);
+    }
+
+    private function loadRoute(bool $force = null):array
+    {
+        $configLoader = ConfigLoader::getInstance();
+        if( !file_exists(ROOT_DIR.'/Var/Cache/App/route.json') || $force || $configLoader->getAppConf()['dev'] ){
+            $modules = $configLoader->listModule();
+            $routes = [];
+            foreach($modules as $module){
+                if($module['enabled']){
+                    $routes = array_merge($routes, $configLoader->loadJsonConfig(ROOT_DIR.'/Module/'.$module['name'].'/Config/Routes/'));
+                }
             }
-            header('Location: /index.php/erreur');
+            file_put_contents(ROOT_DIR.'/Var/Cache/App/route.json', json_encode($routes));
         }
-        header('Location: /index.php/erreur');
+        return json_decode(file_get_contents(ROOT_DIR.'/Var/Cache/App/route.json'), true);
+    }
+
+    private function parse(string $url):?array
+    {
+        $routes = $this->loadRoute();
+        foreach($routes as $route){
+            if($route['path'] == $url) {
+                return $route;
+            }
+        }
+        return null;
+    }
+
+    public function getController()
+    {
+        $start = microtime(true);
+        $route = $this->parse($_GET['url']);
+        if($route){
+            $class = 'Module\\'.$route['module'].'\Controller\\'.$route['controller']; $methode = $route['methode'];
+            $controller = new $class();
+            $end = microtime(true);
+            $controllerstart = microtime(true);
+            $controller->$methode();
+            $controllerend = microtime(true);
+        } else {
+            $end = microtime(true);
+            header('Location: /error', true, 404);
+        }
+        Profiler::getInstance()->setTime(['name'=>'Router', 'start'=>$start, 'end'=>$end]);
+        Profiler::getInstance()->setTime(['name'=>$class.'::'.$methode, 'start'=>$controllerstart, 'end'=>$controllerend]);
     }
 }
--- Public/index.php
+++ Public/index.php
@@ -1,8 +1,15 @@
 <?php
-    require( __DIR__.'/../Core/Kernel.php' );
-    $kernel = new Kernel();
-    use Module\Core\Controller\DefaultController;
-    $instance = new DefaultController();
-    print_r($instance->index());
-    print_r($kernel->loadRoute(true));
+    $start = microtime(true);
+    if(!defined ( 'ROOT_DIR' )){
+        define('ROOT_DIR', __DIR__.'/..');
+        define('WEBROOT_DIR', ROOT_DIR.'/Public');
+    }
+    require_once ROOT_DIR.'/vendor/autoload.php';
+    use Core\Profiler;
+    use Core\Router;
+    $router=new Router();
+    $router->getController();
+    $end = microtime(true);
+    Profiler::getInstance()->setTime(['name'=>'App', 'start'=>$start, 'end'=>$end]);
+    Profiler::getInstance()->profileBar();
 ?>
