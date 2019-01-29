b4eac47170151c64347511f70b2ef9f9217d05ed
GothShoot <romuald.mocq@yahoo.com>
2019-01-29 22:47:50 +0100

    [Improvement] Now you can create internal exeption with NitroException class :smile:

---
+++ BaseClass/Exception.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Alzundaz\NitroPHP\BaseClass;
+
+use Module\ProfilerModule\Services\Profiler;
+
+
+class NitroException
+{
+    public function __construct($type, $message, $file, $line)
+    {
+        Profiler::getInstance()->setError($type, $message, $file, $line);
+    }
+
+    public function __destruct()
+    {
+        // Todo
+    }
+}
