b049a0920d7e4d69bac44c27456da15d5141ebe9
GothShoot <romuald.mocq@yahoo.com>
2019-02-20 15:25:31 +0100

    [Improve] Document and improve Singleton class.

--- BaseClass/Singleton.php
+++ BaseClass/Singleton.php
@@ -2,15 +2,33 @@
 
 namespace Alzundaz\NitroPHP\BaseClass;
 
+/**
+ * Singleton/Multiton Class
+ */
 class Singleton
 {
+    /**
+     * Property used to store instances of classes
+     *
+     * @var array $instances
+     */
     protected static $instances = [];
     
-    final public function __clone()
+    /**
+     * Default cloning protection, you can overide this
+     *
+     * @return void
+     */
+    public function __clone()
     {
         trigger_error("Le clonage n'est pas autorisé.", E_USER_ERROR);
     }
     
+    /**
+     * Singleton instance getters, use Container for multiton
+     *
+     * @return void
+     */
     final public static function getInstance()
     {
         $c = get_called_class();
