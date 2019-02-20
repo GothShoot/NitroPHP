0995f35f2811153e5c0d994b00bd0af77b6b0ed8
GothShoot <romuald.mocq@yahoo.com>
2019-02-20 15:26:02 +0100

    [Improve] New Service container in test.

---
+++ DI/ServicesContainer.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Alzundaz\NitroPHP\DI;
+
+use Alzundaz\NitroPHP\BaseClass\Singleton;
+
+class ServicesContainer extends Singleton
+{
+	/**
+     * @param       $abstract
+     * @param array $parameters
+     *
+     * @return mixed|null|object
+     */
+	public function get($abstract, $parameters = [])
+	{
+		if (!isset($this->instances[$abstract])) {
+			$this->set($abstract);
+		}
+		return $this->resolve($this->instances[$abstract], $parameters);	
+	}
+	
+	/**
+     * resolve single
+     *
+     * @param $concrete
+     * @param $parameters
+     *
+     * @return mixed|object
+     * @throws Exception
+     */
+	public function resolve($concrete, $parameters)
+	{
+		$reflector = new ReflectionClass($concrete);
+
+		if (!$reflector->isInstantiable()) {
+			throw new Exception("Class {$concrete} is not instantiable");
+		}
+
+		$constructor = $reflector->getConstructor();
+
+		if (is_null($constructor)) {
+			return $reflector->newInstance();	
+		}
+
+		$parameters = $constructor->getParameters();
+		$dependencies = $this->getDependencies($parameters);
+
+		return $reflector->newInstanceArgs($dependencies);
+	}
+	
+	/**
+     * get all dependencies resolved
+     *
+     * @param $parameters
+     *
+     * @return array
+     * @throws Exception
+     */
+	public function getDependencies($parameters)
+	{
+		$dependencies = [];
+		foreach ($parameters as $parameter) {
+			$dependency = $parameter->getClass();
+			if ($dependency === null) {
+				if ($parameter->isDefaultValueAvailable()) {
+					$dependencies[] = $parameter->getDefaultValue();
+				}
+				else {
+					throw new Exception("Can not resolve class dependency {$parameter->name}");
+				}
+			}
+			else {
+				$dependencies[] = $this->get($dependency->name);
+			}
+		}
+		return $dependencies;
+	}
+}
+
+
+
+
