ee0046bb97e84dfb1212a650f6faac934a55bade
GothShoot <romuald59161@gmail.com>
2019-07-08 18:07:12 +0200

    [fix] Fix env loading. :wrench:

--- Core/NitroKernel.php
+++ Core/NitroKernel.php
@@ -59,7 +59,7 @@ class NitroKernel extends BaseKernel
             {
                 throw new \RuntimeException('APP_ENV environment variable is not defined. You need to define environment variables for configuration or add "symfony/dotenv" as a Composer dependency to load variables from a .env file.');
             }
-            (new Dotenv())->load($this->getProjectDir().'/../.env');
+            (new Dotenv())->load($this->getProjectDir().'/.env');
         }
     }
 
